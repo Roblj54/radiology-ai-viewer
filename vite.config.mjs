04286d32a8b53e68138766103295ac82989868b7
Roblj54 <128199231+Roblj54@users.noreply.github.com>
2026-02-02 22:14:26 -0500

    chore: pages deploy config

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "/radiology-ai-viewer/",
+  base: '/radiology-ai-viewer/',
   plugins: [react()],
   server: { host: "127.0.0.1", port: 5173, strictPort: false },
   preview: { host: "127.0.0.1", port: 4173, strictPort: false }
